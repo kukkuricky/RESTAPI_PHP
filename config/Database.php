1e53120c983a5acec43af8ddf54d04367f38622c
gitter <gitter@gits.com>
2018-06-01 22:37:24 +0530

    database username and pwd edited

--- config/Database.php
+++ config/Database.php
@@ -1,17 +1,17 @@
-<?php 
+<?php
   class Database {
     // DB Params
     private $host = 'localhost';
-    private $db_name = 'YOURDBNAME';
-    private $username = 'YOURUSERNAME';
-    private $password = 'YOURPASSWORD';
+    private $db_name = 'myblog';
+    private $username = 'root';
+    private $password = 'root';
     private $conn;
 
     // DB Connect
     public function connect() {
       $this->conn = null;
 
-      try { 
+      try {
         $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
